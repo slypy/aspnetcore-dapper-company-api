a0e42cabfac017aa7a4d6f834bc09414c8bb9cb5
Jhon Christian Ambrad <jhonchristian.ambrad@cit.edu>
2022-08-31 21:13:57 +0800

    Add dapper context

---
+++ CompanyApp/CompanyApp/Context/DapperContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace CompanyApp.Context
+{
+    internal class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        internal DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("SqlConnection");
+        }
+
+        public IDbConnection CreateConnection()
+            => new SqlConnection(_connectionString);
+    }
+}
